0c5d554fc7e8544131a3c407f4d3c87ec16afcf3
t0cc <reg-gogs-fcc@agt24.com>
2019-12-02 12:31:04 +0100

    Add initial sources + licence for ArcDPS

---
+++ arcdps_easysetup_tool/arcdps_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "put_arcdps_files_here"
+echo Copying Files....
+cp --verbose "d3d9.dll" "../../data/drive_c/GW2/bin64"
+echo  
+echo arcdps has been installed successfully.
+
---
+++ arcdps_easysetup_tool/arcdps_uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Uninstalling arcdps....
+rm  --verbose "../data/drive_c/GW2/bin64/d3d9.dll"
+echo  
+echo arcdps has been uninstalled successfully.
+
